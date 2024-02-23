facdf195c75e89f4b559d9ea9de1d22dcab3af69
PantofaruTudor <tudorpantofaru@gmail.com>
2024-02-23 13:39:20 +0200

    changes to testgit

--- test_git.cpp
+++ test_git.cpp
@@ -2,8 +2,28 @@
 using namespace std;
 int main()
 {
-    cout<<"Hello world"<<endl;
-    cout<<4;
-
+    int n;
+    cin>>n;
+    int v[n];
+    for(int i=0;i<n;i++)
+        cin>>v[i];
+    int f[n][n];
+    int jos;
+    int sus;
+    for(int i=0;i<n;i++){
+        jos=n-1;
+        sus=0;
+        for(int j=0;j<n;j++){
+            if(i%2==0)
+                f[j][i]=v[jos--];
+            else
+                f[j][i]=v[sus++];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++)
+            cout<<f[i][j]<<" ";
+        cout<<endl;
+        {
     return 0;
 }
