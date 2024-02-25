cc712fd9f8cc43fbf6d2a5d6aeeaa3431f7ac2ed
PantofaruTudor <tudorpantofaru@gmail.com>
2024-02-25 19:29:44 +0200

    modified the test_git.cpp file with a new problem

--- test_git.cpp
+++ test_git.cpp
@@ -1,20 +1,36 @@
 #include<iostream>
-#include<fstream>
+#include<cstring>
 using namespace std;
 int main()
 {
-    ofstream writer("bac.txt");
-    int x;
-    int y;
-    cin>>x>>y;
-    int z=2*x-y+2;
-    writer<<y<<" "<<x<<" "<<z<<" ";
-    while(z!=0){
-        y=x;
-        x=z;
-        z=2*x-y+2;
-        writer<<z<<" ";
+    char c[101];
+    char s[101];
+    cin.getline(s,101);
+    cin.getline(c,101);
+    char vFinal[strlen(c)]="";
+    char v[strlen(c)*2]="";
+    char par[]="02468";
+    int m=strlen(c);
+    int p=m-1;
+    int np=m+1;
+    if(strlen(s)!=strlen(c))
+        cout<<"cod incorect";
+    else{
+        v[m]=s[0];
+        for(int i=1;i<strlen(c);i++){
+            if(strchr(par,c[i])!=NULL){
+                v[p--]=s[i];
+            }
+            else
+                v[np++]=s[i];
+        }
     }
-    writer.close();
+
+    for(int i=0;i<strlen(c)*2;i++)
+        if(v[i]!='0')
+            strcat(vFinal,v+i);
+    cout<<vFinal<< " ";
     return 0;
+    
+
 }
