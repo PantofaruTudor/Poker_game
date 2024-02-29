39ac9d8c4307170b5870dd14670c85dd6af98f3e
PantofaruTudor <tudorpantofaru@gmail.com>
2024-02-29 23:00:59 +0200

    first start to the poker game

--- PokerGame/poker_cards.py
+++ PokerGame/poker_cards.py
@@ -1,33 +1,53 @@
-class Suit():
-    Spades = "\u2660"
-    Hearts = "\u2665"
-    Diamonds = "\2666"
-    Clubs = "\u2663"
-
-class Ranks():
-    TWO = 2
-    THREE = 3
-    FOUR = 4
-    FIVE = 5
-    SIX = 6
-    SEVEN = 7
-    EIGHT = 8
-    NINE = 9 
-    TEN = 10
-    JACK = 11
-    QUEEN = 12
-    KING = 13
-    ACE = 14
-
 class Card():
-    suit : Suit
-    rank : Ranks
+    class Suit():
+        Spades = "\u2660"
+        Hearts = "\u2665"
+        Diamonds = "\2666"
+        Clubs = "\u2663"
 
-    def __str__(self):
-        print
-        return self.rank.value + self.suit.name
+    class Ranks():
+        TWO = 2
+        THREE = 3
+        FOUR = 4
+        FIVE = 5
+        SIX = 6
+        SEVEN = 7
+        EIGHT = 8
+        NINE = 9 
+        TEN = 10
+        JACK = 11
+        QUEEN = 12
+        KING = 13
+        ACE = 14
+    '''
+    value=Ranks
+    name=Suit'''
+    suit: Suit
+    rank: Ranks
 
+    '''def __str__(self,value,name):
+        self.suit=name
+        self.rank=value
+    def show(self):'''
     
+
+Available_card = []
+x = input("how many players?")
+#for i in range x:
+
+
+
+def create_deck() -> List[Card]:
+    return [Card()]
+    
+deck: List[Card]  #PRIMUL PAS: SA CREEZ DECK-UL 
+
+
+class Players():
+    deck=[]    
+    def add_set(self,value,name):
+        
+
 a = Card
 print(a)
         
