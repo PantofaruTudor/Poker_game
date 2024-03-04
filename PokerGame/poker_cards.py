50fa9b7a7f34d717afe0f6156db2dc5adecb6195
PantofaruTudor <tudorpantofaru@gmail.com>
2024-03-04 19:59:06 +0200

    finished the deck of cards and the shuffle

--- PokerGame/poker_cards.py
+++ PokerGame/poker_cards.py
@@ -22,56 +22,19 @@ class Cards():
         KING = 13
         ACE = 14
 
+    #suit:Suit    I SHOULD TRY USING ANNOTATIONS
+    #rank:Ranks
+
     class Card():
         def __init__(self,rank,suit):
             self.suit = suit
             self.rank = rank
-    """
-        def show(self):
-            print("{} of {}".format(self.rank,self.suit))"""
-
-
-
-    card = Card(12,"\u2660")
-    def __init__(self):
         
-        self.deck = []
-        self.create_deck()
+Cards_deck = []
 
-    def create_deck(self):
-        for i in range(14):
-            for s in ["\u2660","\u2665","\u2666","\u2663"]:
-                self.deck.append(Cards.Card(i,s))
-    
-    def Show_deck(self):
-        for i in self.deck:
-            print("{} of {}".format(i.rank,i.suit))
-    
-
-
-    
-Cards_deck=Cards()
-Cards_deck.Show_deck()
-Available_card = []
+for i in range(1,14):
+    for s in ["\u2660","\u2665","\u2666","\u2663"]:
+        Cards_deck.append(Cards.Card(i,s))
 
 
 
-#x = input("how many players?")
-#for i in range x:
-#random.shuffle(Deck) 
-
-
-#def create_deck() -> List[Card]:
- #   return [Card()]
-    
-#deck: List[Card]  #PRIMUL PAS: SA CREEZ DECK-UL 
-
-"""
-class Players():
-    deck=[]    
-    def add_set(self,value,name):
-    """    
-
-#a = Card
-#print(a)
-        
--- PokerGame/poker_game.py
+++ PokerGame/poker_game.py
@@ -1,22 +1,29 @@
-import asyncio
-import _random
-from poker_cards import Players,Cards_deck
-print("\U0000E5A5BD")
+import random
+from poker_cards import Cards_deck
 
-class Cards:
-    TWO = 2
-    THREE = 3
-    FOUR = 4
-    FIVE = 5
-    SIX = 6
-    SEVEN = 7
-    EIGHT = 8
-    NINE = 9 
-    TEN = 10
-    JACK = 11
-    QUEEN = 12
-    KING = 13
-    ACE = 14
-#class Player:
+class player():
+    def __init__(self,bet):
+        self.bet = bet
+        self.deck = []
 
-def CreateDeck() -> list[Cards]:
+winner = False
+n=int(input("Welcome to the game. How many players will join?"))
+bet = 2
+Players = [player]
+for i in range(1,n+1):
+    p = player
+    p.bet = int(input(f"Bet for player {i}: "))
+    p.deck = []
+    Players.append(p)
+
+random.shuffle(Cards_deck)
+community_cards = []
+
+"""
+for i in range(len(Cards_deck)):
+    print(f"{Cards_deck[i].rank} of {Cards_deck[i].suit}")"""
+
+#while winner:
+
+#def Pre_flop():
+
