26120a0355473da3a6714b36e99cdaf49fb1f315
PantofaruTudor <tudorpantofaru@gmail.com>
2024-03-10 22:03:10 +0200

    finished pre-flop

--- PokerGame/poker_game.py
+++ PokerGame/poker_game.py
@@ -1,30 +1,35 @@
 import random
 import time
-from poker_cards import Cards_deck
+from poker_cards import Cards_deck,Cards
 
 class player():
     def __init__(self,bet):
         self.bet = bet
         self.deck = []
-
+    def inList(self,card):
+        self.deck.append(card)
+    def show(self):
+        print(self.deck[0].suit,self.deck[0].rank)
 winner = False
 n=int(input("Welcome to the game. How many players will join?"))
 bet = 2
 Players = [player]
 for i in range(1,n+1):
-    pl = player
-    pl.bet = int(input(f"Bet for player {i}: "))
-    flop = random.choice(Cards_deck)
-    Cards_deck.remove(flop)
-    pl.deck.append(flop)
+    pl = player(int(input(f"Bet for player {i}: ")))
     Players.append(pl)
-    print(f"Player {i} has {Players[i].deck[0].rank} of {Players[i].deck[0].suit}")
-
+    
 random.shuffle(Cards_deck)
 community_cards = []
 
-"""
-for i in range(len(Cards_deck)):
-    """
-
 #----------------------------------------------PRE-FLOP
+
+for i in range(1,n+1):
+    for j in range(2):
+        flop = random.choice(Cards_deck)
+        Players[i].inList(flop)
+        Cards_deck.remove(flop)
+    #Players[i].show() ASTA E PRE-FLOP, FIECARE PLAYER ARE PRIMELE 2 CARTI
+
+#----------------------------------------------
+        
+
