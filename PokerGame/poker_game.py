e7a48d6be0cf766b0051424645b67f0178ec6b63
PantofaruTudor <tudorpantofaru@gmail.com>
2024-03-11 21:30:40 +0200

    testing the flop and next rounds

--- PokerGame/poker_game.py
+++ PokerGame/poker_game.py
@@ -9,27 +9,55 @@ class player():
     def inList(self,card):
         self.deck.append(card)
     def show(self):
-        print(self.deck[0].suit,self.deck[0].rank)
+        for i in range(2):
+            print(self.deck[i].suit,self.deck[i].rank)
+
+def Flop(deck,dealer):
+    for _ in range(3):
+        card = random.choice(deck)
+        dealer.append(card)
+        deck.remove(card)
+
+def Pre_flop(deck,players_list):
+    for i in range(1,n+1):
+        for j in range(2):
+            card = random.choice(deck)
+            players_list[i].inList(card)
+            deck.remove(card)
+
 winner = False
-n=int(input("Welcome to the game. How many players will join?"))
-bet = 2
+n = int(input("Welcome to the game. How many players will join?"))
+min_bet = 2
+Pot = []
 Players = [player]
+Dealer = []
+
 for i in range(1,n+1):
-    pl = player(int(input(f"Bet for player {i}: ")))
+    Pot.append(int(input(f"Pot for player {i} :")))
+
+for i in range(1,n+1):
+    pl = player(min_bet)
+    Pot[i] -= min_bet
     Players.append(pl)
+
     
-random.shuffle(Cards_deck)
-community_cards = []
+random.shuffle(Cards_deck) 
 
-#----------------------------------------------PRE-FLOP
+Pre_flop(Cards_deck,Players)
+Flop(Cards_deck,Dealer)
 
-for i in range(1,n+1):
-    for j in range(2):
-        flop = random.choice(Cards_deck)
-        Players[i].inList(flop)
-        Cards_deck.remove(flop)
-    #Players[i].show() ASTA E PRE-FLOP, FIECARE PLAYER ARE PRIMELE 2 CARTI
+CALL = False
+CHECK = False
+FOLD = False
+RAISE = False
+MIN_BET = True
 
-#----------------------------------------------
-        
-
+while winner == False:
+    while MIN_BET:
+        for i in range(1,n+1):
+            bet = int(input(f"Bet for player{i}:"))
+            if bet < min_bet:
+                print("Please enter a higher or equal bet.")
+                break
+            else:
+                Players[i]
