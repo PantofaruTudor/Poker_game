5915a4840f02af323355344e4e1ee92decdfc4ef
PantofaruTudor <tudorpantofaru@gmail.com>
2024-03-29 22:51:51 +0200

    working on the royal flush

--- PokerGame/poker_hand_rankings.py
+++ PokerGame/poker_hand_rankings.py
@@ -1,13 +1,22 @@
 #THIS IS WHERE I WILL MAKE THE RANKINGS
 from poker_game import Players,n,Dealer
 
-
+def deck_merge(DL_deck,PL_deck):
+    mrg_deck = []
+    n = len(DL_deck)
+    m = len(PL_deck)
+    i,j = 0
+    while i != n and j != m:
+        if DL_deck[i]<PL_deck[j]:
+            mrg_deck.append(DL_deck[i])
+            
 def Royal_Flush(DL_deck,PL_deck,index):
     royal = True
-    deck = []
-    suit = None
-    for i in range(5):
-        if 
+    i = 0
+    j = 0
+    
+        
+            
         
         
 
