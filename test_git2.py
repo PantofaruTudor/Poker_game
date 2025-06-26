63e52c1b03c8cfa809079dade07ca746d61c902c
PantofaruTudor <157917631+PantofaruTudor@users.noreply.github.com>
2025-06-27 01:56:16 +0300

    Delete test_git2.py

--- test_git2.py
+++
@@ -1,6 +0,0 @@
-import random
-number=input("How many characters the password needs to be?")
-complexity=input("How complex it needs to be(medium,hard,easy)?")
-
-NUM={0,1,2,3,4,5,6,7,8,9}
-#def generate_password(number,complexity,)
